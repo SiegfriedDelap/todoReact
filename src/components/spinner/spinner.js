1ef6967ce2f2b776ce2c7acc1897191272bac251
YueJin <YueJin07@gmail.com>
2021-08-26 16:53:48 +0300

    Loading logic + view component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () =>{
+    return(
+        <div className="wrapper">
+            <div className="loadingio-spinner-double-ring-1j6qayx37b8h">
+                <div className="ldio-wbqjr2ut2ob">
+                    <div></div>
+                    <div></div>
+                    <div><div></div></div>
+                    <div><div></div></div>
+                </div> 
+            </div>
+        </div>
+       
+    );
+};
+
+export default Spinner;
