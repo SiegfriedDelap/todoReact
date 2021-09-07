f35a6d2506128cc0930be01ffd9df08adf2c4a46
YueJin <YueJin07@gmail.com>
2021-09-07 15:20:48 +0300

    First  coomit reducer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {createStore} from 'redux';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const reducer = (state=0, action)=> {
+
+
+	switch(action.type) {
+		case 'INC':
+			return state+1;
+		default: 
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+store.subscribe(()=>{
+	console.log(store.getState());
+});
+
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
