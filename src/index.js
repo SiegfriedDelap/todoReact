9bcfff53a1df896bb79c4a87cf942896d08fc304
YueJin <YueJin07@gmail.com>
2021-09-10 15:17:00 +0300

    reducer + html

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 
-import {createStore} from 'redux';
-
-const reducer = (state=0, action)=> {
-
-
-	switch(action.type) {
-		case 'INC':
-			return state+1;
-		default: 
-			return state;
-	}
-};
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './actions';
 
 const store = createStore(reducer);
-store.subscribe(()=>{
-	console.log(store.getState());
+const {dispatch} = store;
+
+
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+ 
+document.getElementById("inc").addEventListener("click", inc);
+ 
+document.getElementById("dec").addEventListener("click", dec);
+ 
+document.getElementById("rnd").addEventListener("click", () => {
+  const payload = Math.floor(Math.random() * 11);
+  rnd(payload);
 });
 
+const update = () => {
+	document.getElementById('counter').innerHTML = store.getState();
+}
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+store.subscribe(update)
+
+
+
