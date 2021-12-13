8c341f916605d9a15e84029dfe89510247a092c1
YueJin <YueJin07@gmail.com>
2021-12-13 19:14:11 +0200

    pages in progress

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,19 @@
 import React, {Component} from "react";
 import Spinner from "../spinner";
 import {withBookstoreService} from '../hoc';
+import {Route, Router, Switch} from 'react-router-dom';
+import {HomePage, CartPage} from '../pages';
+
 
 const App = ({bookstoreService}) => {
     console.log(bookstoreService.getBooks());
     return (
-        <Spinner/>
+        <Switch>
+            <Route 
+                path="/"
+                component={HomePage}
+            />
+        </Switch>
     )
 };
 
