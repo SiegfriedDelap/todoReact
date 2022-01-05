4955ca0eb78781fd6139a17e7bef5ce8d13a3633
Siegfried <pavlo.laptiev@gmail.com>
2022-01-05 17:46:58 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 class App {
-    run() {
-        const name = "World";
+
+    run = async (name = "World") => {
         console.log(`Hello ${name}`);
     }
 }
 
 const app = new App();
-app.run();
+app.run()
+    .then(()=>console.log('done'))
+    .catch(()=>console.log('error'));
