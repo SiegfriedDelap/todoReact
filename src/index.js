33176165795cbde456e4fbf7fa7da898e740fd08
Siegfried <pavlo.laptiev@gmail.com>
2022-01-06 16:57:52 +0200

    webpack-dev-server; npm run build

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
 
-const App = () => <p>Hello world!</p>
+const App = () => <p>React Webpack; Hello world!</p>
 ReactDOM.render(<App/>, document.getElementById('root'));
